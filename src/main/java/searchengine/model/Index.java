775b943dffaa1b5d2ad3bc303daba800afb4b0cc
dmitrii_kuznetsov_4 <dmitriy.smit@mail.ru>
2023-07-06 18:11:13 +0300

    last changes 6/07/23 dependency wrong

---
+++ src/main/java/searchengine/model/Index.java
@@ -0,0 +1,49 @@
+package searchengine.model;
+
+import javax.persistence.*;
+
+@Table(name="index",schema = "search_engine")
+@Entity
+public class Index {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column  (name="page_id",nullable = false)
+    private  int pageId;
+    @Column  (name="lemma_id",nullable = false)
+    private  int lemmaId;
+    @Column  (name="rank",nullable = false)
+    private  float rank;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPageId() {
+        return pageId;
+    }
+
+    public void setPageId(int pageId) {
+        this.pageId = pageId;
+    }
+
+    public int getLemmaId() {
+        return lemmaId;
+    }
+
+    public void setLemmaId(int lemmaId) {
+        this.lemmaId = lemmaId;
+    }
+
+    public float getRank() {
+        return rank;
+    }
+
+    public void setRank(float rank) {
+        this.rank = rank;
+    }
+}
