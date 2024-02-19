a26661702c9a26db4b8363bcb3e175dadccee056
dmitrii_kuznetsov_4 <dmitriy.smit@mail.ru>
2024-02-19 17:56:37 +0300

    last changes 19/02/24. Corrected.

--- src/main/java/searchengine/services/StatisticsServiceImpl.java
+++ src/main/java/searchengine/services/StatisticsServiceImpl.java
@@ -3,6 +3,7 @@ package searchengine.services;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import searchengine.config.Site;
 import searchengine.config.SitesList;
 import searchengine.dto.statistics.DetailedStatisticsItem;
 import searchengine.dto.statistics.StatisticsData;
@@ -33,32 +34,61 @@ public class StatisticsServiceImpl implements StatisticsService {
 
     @Override
     public StatisticsResponse getStatistics() {
-        TotalStatistics total = new TotalStatistics();
-        if (siteModelRepository.count()==0){
+        //       TotalStatistics total = new TotalStatistics();
+        if (siteModelRepository.count() == 0) {
+            TotalStatistics total = new TotalStatistics();
             total.setSites(sites.getSites().size());
-        }else {
-        total.setSites((int) siteModelRepository.count());
-        }
-        total.setIndexing(true);
+            total.setIndexing(true);
+            List<DetailedStatisticsItem> detailed = new ArrayList<>();
+            StatisticsData data = new StatisticsData();
+            List<Site> sitesList = sites.getSites();
+            for (int i = 0; i < sitesList.size(); i++) {
+                Site site = sitesList.get(i);
+                DetailedStatisticsItem item = new DetailedStatisticsItem();
+                item.setName(site.getName());
+                item.setUrl(site.getUrl());
+                int pages = 0;
+                int lemmas = 0;
+                item.setPages(pages);
+                item.setLemmas(lemmas);
+                item.setStatus(null);
+                item.setError(null);
+                item.setStatusTime(null);
+                total.setPages(0);
+                total.setLemmas(0);
+                detailed.add(item);
+                data.setDetailed(detailed);
+                data.setTotal(total);
+            }
+            StatisticsResponse response = new StatisticsResponse();
+            response.setStatistics(data);
+            response.setResult(true);
 
-       List<DetailedStatisticsItem> detailed = new ArrayList<>();
-        StatisticsData data = new StatisticsData();
-            Iterable<SiteModel> siteResult=siteModelRepository.findAll();
-            Iterable<Page> pageResult=pageRepository.findAll();
-            Iterable<Lemma> lemmaResult=lemmaRepository.findAll();
-            for(SiteModel s:siteResult) {
+            return response;
+        } else {
+            TotalStatistics total = new TotalStatistics();
+            total.setSites((int) siteModelRepository.count());
+
+            total.setIndexing(true);
+
+            List<DetailedStatisticsItem> detailed = new ArrayList<>();
+            StatisticsData data = new StatisticsData();
+            Iterable<SiteModel> siteResult = siteModelRepository.findAll();
+            Iterable<Page> pageResult = pageRepository.findAll();
+            Iterable<Lemma> lemmaResult = lemmaRepository.findAll();
+            for (SiteModel s : siteResult) {
                 DetailedStatisticsItem item = new DetailedStatisticsItem();
                 item.setName(s.getName());
                 item.setUrl(s.getUrl());
-                int pages=0;
-                int lemmas =0;
-                for(Page p:pageResult) {
-                    if (p.getSiteId()==s.getId()){
-                     pages++;
+                int pages = 0;
+                int lemmas = 0;
+                for (Page p : pageResult) {
+                    if (p.getSiteId() == s.getId()) {
+                        pages++;
                     }
                 }
-                for(Lemma l:lemmaResult) {
-                    if (l.getSiteId()==s.getId()){
+                for (Lemma l : lemmaResult) {
+                    if (l.getSiteId() == s.getId()) {
                         lemmas++;
                     }
                 }
@@ -68,15 +98,18 @@ public class StatisticsServiceImpl implements StatisticsService {
                 item.setError(s.getLastError());
                 item.setStatusTime(s.getStatusTime());
 
-            total.setPages(total.getPages() + pages);
-            total.setLemmas(total.getLemmas() + lemmas);
-            detailed.add(item);
-            data.setDetailed(detailed);
-            data.setTotal(total);
+                total.setPages(total.getPages() + pages);
+                total.setLemmas(total.getLemmas() + lemmas);
+                detailed.add(item);
+                data.setDetailed(detailed);
+                data.setTotal(total);
+            }
+
+            StatisticsResponse response = new StatisticsResponse();
+            response.setStatistics(data);
+            response.setResult(true);
+
+            return response;
         }
-        StatisticsResponse response = new StatisticsResponse();
-        response.setStatistics(data);
-        response.setResult(true);
-        return response;
     }
 }
